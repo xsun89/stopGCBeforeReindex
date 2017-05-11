e3b685e652fc9ec3bcf5d96b27a95277f5bf4145
xsun <xsun@XINSUN3E32>
2017-05-11 10:15:05 -0700

    Add project files.

---
+++ StopGCBeforReindex/Program.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace StopGCBeforReindex
+{
+    class Program
+    {
+        private static readonly bool IsDebug = bool.Parse(ConfigurationManager.AppSettings["isDebug"]);
+        private static string womDirStr = @"F:\Logs\Click Portal";
+        //private static string womDirStr = @"C:\Temp\stopGC";
+
+        private static string Keyword =
+                @"^\d{4}.\d{2}.\d{2}\s\d{2}:\d{2}:\d{2}\s\(\w+\):\s\w+:\sGarbageCollection\s\(\w+\)\s-\sDeleted\s(?<current>\d+)\sof\s(?<total>\d+)\srows.$"
+            ;
+
+        private static string stopGCFile = @"C:\Schedulled Tasks\stopGC.bat";
+        private static readonly int Interval = int.Parse(ConfigurationManager.AppSettings["interval"]);
+        private static readonly int IntervalForCheck = Interval * 1000;
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                LogMessageToFile("Start Check GC Status");
+                FileInfo result = null;
+                result = GetLatestWomLogFile(womDirStr);
+                LogMessageToFile("processing log file " + result.FullName);
+                var dayofToday = DateTime.Now.DayOfWeek;
+                if (dayofToday.ToString() == "Saturday" || IsDebug)
+                {
+                    while (true)
+                    {
+                        LogMessageToFile("Check GC Status");
+                        bool ret = GetKewwordLines(result.FullName);
+                        if (ret == true && (System.DateTime.Now.Hour >= 8 || IsDebug))
+                        {
+                            LogMessageToFile("Preparing Stop GC at day of hour " + System.DateTime.Now.Hour);
+                            break;
+                        }
+                        Thread.Sleep(IntervalForCheck);
+                    }
+                    ProcessStopGc();
+                }
+                LogMessageToFile("End Check GC Status");
+            }
+            catch (Exception e)
+            {
+                LogMessageToFile(e.Message);
+            }
+        }
+
+        private static bool GetKewwordLines(string fullName)
+        {
+            try
+            {
+                
+                List<string> allLines = new List<string>();
+                using (var filestream = new FileStream(fullName, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+                {
+                    using (var file = new StreamReader(filestream))
+                    {
+                        string oneLine;
+                        while ((oneLine = file.ReadLine()) != null)
+                        {
+                            allLines.Add(oneLine);
+                        }
+                    }
+                }
+
+            
+                
+                var regex = new Regex(Keyword);
+
+                foreach (string line in allLines)
+                {
+                    Match match = regex.Match(line);
+                    if (match.Success)
+                    {
+                        string current = match.Groups["current"].Value;
+                        string total = match.Groups["total"].Value;
+                        int currentVal = int.Parse(current);
+                        int totalVal = int.Parse(total);
+                        if (IsDebug)
+                        {
+                            LogMessageToFile(currentVal + " " + totalVal);
+                        }
+                        if (totalVal - currentVal <= 2000)
+                        {
+                            LogMessageToFile(currentVal + " " + totalVal);
+                            return true;
+                        }
+                    }
+                }
+                LogMessageToFile("Keep processing");
+                return false;
+            }
+            catch (Exception e)
+            {
+                LogMessageToFile(e.Message);
+                throw;
+            }
+        }
+
+        private static void ProcessStopGc()
+        {
+            try
+            {
+                LogMessageToFile("start stop GC");
+
+                using (Process stopGc = Process.Start(stopGCFile))
+                {
+                    if (stopGc != null) stopGc.WaitForExit();
+                }
+                LogMessageToFile("end stop GC");
+            }
+            catch (Exception e)
+            {
+                LogMessageToFile(e.Message);
+                throw;
+            }
+        }
+
+        private static DateTime GetTimeStamp(List<string> lines)
+        {
+            var lastItem = lines.LastOrDefault();
+            var lastDateTime = DateTime.MinValue;
+
+            if (lastItem != null)
+            {
+                LogMessageToFile(lastItem);
+                var year = int.Parse(lastItem.Substring(0, 4));
+                var month = int.Parse(lastItem.Substring(5, 2));
+                var day = int.Parse(lastItem.Substring(8, 2));
+                var hour = int.Parse(lastItem.Substring(11, 2));
+                var mim = int.Parse(lastItem.Substring(14, 2));
+                var sec = int.Parse(lastItem.Substring(17, 2));
+                lastDateTime = new DateTime(year, month, day, hour, mim, sec);
+            }
+            return lastDateTime;
+        }
+
+        private static FileInfo GetLatestWomLogFile(string womdir)
+        {
+            FileInfo result = null;
+            var directory = new DirectoryInfo(womdir);
+            var list = directory.GetFiles("wom*.log");
+            if (list.Any())
+            {
+                result = list.OrderByDescending(f => f.LastWriteTime).First();
+            }
+
+            return result;
+        }
+
+        private static void LogMessageToFile(string msg)
+        {
+            var sw = File.AppendText("LogFile.txt");
+            try
+            {
+                string logLine = string.Format(
+                    "{0:G}: {1}.", DateTime.Now, msg);
+                sw.WriteLine(logLine);
+            }
+            finally
+            {
+                sw.Close();
+            }
+        }
+    }
+}
